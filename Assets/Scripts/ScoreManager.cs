5b9307572f3da8074950978344af44a9832c0a8e
glenn-git <glennsularko@gmail.com>
2025-11-22 01:43:25 +0800

    added upgrade click function
    no cost yet

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public int score = 0; // Score counter
+    public int scorePerClick = 1;
     public TMP_Text scoreText; // UI text to display score
 
     // method to add score
     public void AddScore()
     {
-        score++;
+        score += scorePerClick;
         UpdateScoreText();
     }
     void UpdateScoreText()
     {
         scoreText.text = "Score: " + score;
+        Debug.Log("Score: " + score);
+    }
+    public void UpgradeClick()
+    {
+        scorePerClick += 1;
+        Debug.Log("Score per click: " + scorePerClick);
     }
 }
