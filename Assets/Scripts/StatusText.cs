7b7d6ee8a1d87eacebd330b449e8c2855b1ea844
glenn-git <glennsularko@gmail.com>
2025-12-09 08:46:30 +0800

    edited upgrade

--- Assets/Scripts/StatusText.cs
+++ Assets/Scripts/StatusText.cs
@@ -6,6 +6,6 @@ public class StatusText : MonoBehaviour
     public TMP_Text StatusTMP;
     public void DisplayStatus()
     {
-        StatusTMP.text = "Hello";
+        StatusTMP.text = "Use Click Me button to start earning points!";
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 //Glenn
 //M122777
@@ -9,14 +10,21 @@ public class UIManager : MonoBehaviour
     [Header("Player Stats and game settings")]
     public ScoreManager scoreManager;
     public int costMultiplier = 10;
-
+    public float gameTime = 0f;
+    private float perSecond = 1f;
+    
     [Header("UI interactions")]
     public Button clickerButton;
     public Button upgradeButton;
+    public Button autoUpgradeButton;
     public Slider upgradeSlider;
 
     [Header("UI labels")]
-    public TMP_Text scoreText; // UI text to display score (Status Text)
+    public TMP_Text gameTimeText;
+    public TMP_Text scoreText; // UI text to display score
+    public TMP_Text statusText; // UI text to display status
+    public TMP_Text startButtonText; // UI text to display start / pause
+
     public TMP_Text effectPerUpgradeText; //UI text to display upgrade effect
     public TMP_Text costPerUpgradeText; //UI text to display upgrade cost
     public TMP_Text scorePerClickText; //UI text to display current per click value
@@ -25,10 +33,16 @@ public class UIManager : MonoBehaviour
     public TMP_Text costPerAutoUpgradeText; //UI text to display auto upgrade cost
     public TMP_Text scorePerAutoClickText; //UI text to display current per auto click value
 
-    public void ToggleClickerButton()
+    public void ToggleButtons()
     {
-        // Toggle active state
-        clickerButton.gameObject.SetActive(!clickerButton.gameObject.activeSelf);
+        bool isActive = !clickerButton.gameObject.activeSelf; // invert current state
+        clickerButton.gameObject.SetActive(isActive);
+        upgradeButton.gameObject.SetActive(isActive);
+        autoUpgradeButton.gameObject.SetActive(isActive);
+        upgradeSlider.gameObject.SetActive(isActive);
+
+        // Update Start button text
+        startButtonText.text = isActive ? "Pause" : "Start"; // ternary conditional operator, shorthand for if-else statement condition ? valueiftrue : valueiffalse.
     }
     // Button click methods
     public void OnClickerButtonClick()
@@ -48,11 +62,12 @@ public class UIManager : MonoBehaviour
             scoreManager.score -= cost;
             scoreManager.AddScorePerClick(upgradeAmount);
             UpdateScorePerClick();
-        } 
+            UpdateScore();
+        }
         else
         {
             Debug.Log("Not enough score to upgrade!");
-            scoreText.text = "Not enough score to upgrade!";
+            statusText.text = "Not enough score to upgrade!";
         }
     }
     public void OnAutoUpgradeButtonClick()
@@ -64,11 +79,12 @@ public class UIManager : MonoBehaviour
             scoreManager.score -= cost;
             scoreManager.AddScorePerAutoClick(upgradeAmount);
             UpdateScorePerAutoClick();
+            UpdateScore();
         }
         else
         {
             Debug.Log("Not enough score to upgrade!");
-            scoreText.text = "Not enough score to upgrade!";
+            statusText.text = "Not enough score to upgrade!";
         }
     }
     // Update methods to display labels
@@ -97,6 +113,24 @@ public class UIManager : MonoBehaviour
         effectPerAutoUpgradeText.text = $"Score +{upgradeAmount} per second";
         costPerAutoUpgradeText.text = $"Cost: {cost}";
     }
+    //public void SaveScore()
+    //{
+    //    string directoryPath = Application.dataPath + "/DataFiles/";
+    //    string filePath = directoryPath + saveFileName + ".csv";
+
+    //    // Ensure folder exists
+    //    if (!Directory.Exists(directoryPath))
+    //        Directory.CreateDirectory(directoryPath);
+
+    //    // CSV header
+    //    string csv = "Score,ScorePerClick,ScorePerAutoClick\n";
+    //    // CSV data
+    //    csv += $"{score},{scorePerClick},{scorePerAutoClick}\n";
+
+    //    File.WriteAllText(filePath, csv);
+    //    Debug.Log("Saved score to: " + filePath);
+    //}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -106,9 +140,23 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameTime += Time.deltaTime;
+        perSecond -= Time.deltaTime;
+
+        gameTimeText.text = gameTime.ToString("F2"); // F2 is 2 decimal places
+        //If timer is 0, add autoclicker score and reset timer
+        if (perSecond <= 0f)
+        {
+            scoreManager.score += scoreManager.scorePerAutoClick;
+            UpdateScore();
+            perSecond = 1f;
+        }
     }
 }
 /* Notes
+ * Time https://youtu.be/1bV3BlRvADE?t=55
  * Naming convention - prefix method with on. https://unity.com/how-to/naming-and-code-style-tips-c-scripting-unity
+ * sidebar tutorial + basic cookie clicker https://www.youtube.com/watch?v=i2XnYFCkpr8
+ * unused yet
+ * slider script https://youtu.be/FMy5ixxBlYw?t=389
  */
